a03df39069ba7e5afb9b49c16e3b0b4025dc2fab
Aslan <asl.zurabov@gmail.com>
2023-01-16 12:50:24 +0300

    checkbox setting in state

--- src/CheckBox/CheckBox.js
+++ src/CheckBox/CheckBox.js
@@ -2,16 +2,17 @@ import React from "react";
 import PropTypes from "prop-types";
 import classes from "./CheckBox.module.scss";
 
-const CheckBox = ({ text, disabled, checked }) => {
+const CheckBox = ({ text, disabled, checked, value, onCheckChange }) => {
   return (
     <label className={`${classes.control} ${classes.control_checkbox}`}>
       {text}
       <input
         type="checkbox"
         onChange={(e) => {
-          console.log(e);
+          onCheckChange(e);
         }}
         disabled={disabled}
+        value={value}
         checked={checked}
       />
       <div className={classes.control_indicator} />
@@ -21,12 +22,16 @@ const CheckBox = ({ text, disabled, checked }) => {
 CheckBox.defaultProps = {
   text: "",
   disabled: "",
-  checked: false,
+  checked: "",
+  value: [],
+  onCheckChange: () => {},
 };
 
 CheckBox.propTypes = {
   text: PropTypes.string,
-  checked: PropTypes.bool,
+  value: PropTypes.array,
+  checked: PropTypes.string,
   disabled: PropTypes.string,
+  onCheckChange: PropTypes.func,
 };
 export default CheckBox;
--- src/Filter/Filter.js
+++ src/Filter/Filter.js
@@ -1,48 +1,59 @@
 import React, { useState } from "react";
-// import CheckBox from "../CheckBox/CheckBox";
-import { Checkbox } from "antd";
+import CheckBox from "../CheckBox/CheckBox";
 import classes from "./Filter.module.scss";
 
-const CheckboxGroup = Checkbox.Group;
 const plainOptions = [
-  "Без пересадок",
-  "1 пересадка",
-  "2 пересадки",
-  "3 пересадки",
+  ["Без пересадок", 0],
+  ["1 пересадка", 1],
+  ["2 пересадки", 2],
+  ["3 пересадки", 3],
 ];
-const defaultCheckedList = ["1 пересадка", "2 пересадки"];
+const defaultCheckedList = { "1 пересадка": "1", "2 пересадки": "2" };
 const Filter = () => {
   const [checkedList, setCheckedList] = useState(defaultCheckedList);
-  const [indeterminate, setIndeterminate] = useState(true);
-  const [checkAll, setCheckAll] = useState(false);
-  const onChange = (list) => {
-    setCheckedList(list);
-    setIndeterminate(!!list.length && list.length < plainOptions.length);
-    setCheckAll(list.length === plainOptions.length);
+  const [checkAll, setCheckAll] = useState("");
+  const onChange = (e) => {
+    const { checked, value } = e.target;
+    const [name, val] = value.split(",");
+    if (checked) {
+      setCheckedList({ ...checkedList, ...{ [name]: val } });
+    } else {
+      const list = { ...checkedList };
+      delete list[name];
+      setCheckedList(list);
+    }
+
+    // setCheckAll(list.length === plainOptions.length);
   };
   const onCheckAllChange = (e) => {
-    setCheckedList(e.target.checked ? plainOptions : []);
-    setIndeterminate(false);
-    setCheckAll(e.target.checked);
+    setCheckedList(e.target.checked ? Object.fromEntries(plainOptions) : {});
+    setCheckAll(e.target.checked ? "checked" : "");
   };
   return (
     <div className={classes.filterContainer}>
       <div className={classes.filter}>
         <h3 className={classes.header}>Количество пересадок</h3>
 
-        <Checkbox
-          indeterminate={indeterminate}
-          onChange={onCheckAllChange}
+        <CheckBox
+          onCheckChange={onCheckAllChange}
           checked={checkAll}
-        >
-          Все
-        </Checkbox>
-        <CheckboxGroup
-          className="check-group"
-          options={plainOptions}
-          value={checkedList}
-          onChange={onChange}
+          text="Все"
         />
+
+        {plainOptions.map((option) => {
+          const [name, value] = option;
+          const checked = checkedList[name] > -1 ? "checked" : "";
+
+          return (
+            <CheckBox
+              onCheckChange={onChange}
+              key={value}
+              value={option}
+              text={name}
+              checked={checked}
+            />
+          );
+        })}
       </div>
     </div>
   );
